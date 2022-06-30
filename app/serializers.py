4a738b4ed83d6be4aa71a6545c1447adbf2cc96e
world970511 <57740138+world970511@users.noreply.github.com>
2022-06-30 15:58:02 +0900

    feat:DRF연동(1) 모델 생성

---
+++ app/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User
+
+class UserCreateSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    print(email)
+    def create(self, validated_data):
+        user = User.objects.create( # User 생성
+            email=validated_data['email'],
+            username=validated_data['username'],
+        )
+        user.set_password(validated_data['password'])
+
+        user.save()
+        return user
